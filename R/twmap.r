96caac90062037dc893cc3660a14187670af1056
Lin, Cheng-Tao <mutolisp@gmail.com>
2011-05-27 21:32:03 +0800

    Add draw vertical map function: draw.vertmap()

--- R/twmap.r
+++ R/twmap.r
@@ -154,3 +154,130 @@ twcoor.trans <- function(coords, src, dst){
      }
      else print("Exception caught! Please report a bug! Thank you")
 }
+
+draw.vertmap <- function(coorsys, orient=1, mountain=T) {
+    data(elevprof)
+    # transformation of wgs84 and twd97
+    proflong <- rep(20000, dim(elevprof)[1])
+    elevprof84 <- cbind(twcoor.trans(as.matrix(cbind(proflong, elevprof[,1])), 97, 84)[, 2], elevprof[, 2])
+    # orientation selection
+    if ( orient == 1 ) {
+        o.side1 <- 1
+        o.side2 <- 2
+        o.ylab <- "Elevation (m)"
+        # coordinate system selection
+        if ( coorsys == 84 ) {
+            elev <- cbind(elevprof84[, 1], elevprof84[, 2])
+            o.xlim <- c(25.7,21.5)
+            o.ylim <- c(0, 4000)
+            o.xlab <- "Latitude"
+            latdeg <- parse(text=paste(25:22,"*degree~", "N", sep=""))
+            gridv <- 25:22
+            gridv.sub <- 215:257/10 
+            datun.x <- 25.2
+            datun.y <- 1200
+            hsueh.x <- 24.35
+            hsueh.y <- 4000
+            yushan.x <- 23.45
+            yushan.y <- 4000
+            beidawu.x <- 22.6
+            beidawu.y <- 3200
+        } else if ( coorsys == 97 ) {
+            elev <- cbind(elevprof[, 1], elevprof[, 2])
+            o.xlim <- c(2850000,2380000)
+            o.ylim <- c(0, 4200)
+            o.xlab <- "TWD97 TM2 Y (meter)"
+            latdeg <- c(28:24*100000)
+            gridv <- 28:24*100000
+            gridv.sub <- 235:287*10000 
+            datun.x <- 2790000
+            datun.y <- 1200
+            hsueh.x <- 2700000
+            hsueh.y <- 4000
+            yushan.x <- 2600000
+            yushan.y <- 4000
+            beidawu.x <- 2500000 
+            beidawu.y <- 3200
+        } else print("Unsupported coordinate system!")
+
+    quartz(width=12,height=5)
+    plot(elev, pch=".", ylim=o.ylim, xlim=o.xlim, axes=F,
+                        ylab=o.ylab, xlab=o.xlab)
+    lines(elev)
+    abline(h=c(0:4)*(1000), b=0, lwd=1.2, col="grey")
+    abline(h=c(0:42)*(100), b=0, lwd=0.5, col="grey")
+    abline(v=gridv,lwd=1.2,col="grey")
+    abline(v=gridv.sub,lwd=0.5,col="grey")
+    # draw axis
+    axis(side=o.side1, gridv, latdeg, las=1)
+    axis(side=o.side2, 1000*(0:4), cbind("0","1000","2000","3000","4000"))
+    # draw frame
+    box()
+    if ( mountain == T ) {
+        text(datun.x,datun.y, labels='Datun Shan')
+        text(hsueh.x,hsueh.y, labels='Hsueh Shan')
+        text(yushan.x,yushan.y, labels='Yu Shan')
+        text(beidawu.x,beidawu.y, labels='BeiDawu Shan')
+    }
+
+    } else if ( orient == 2 ){
+        o.side1 <- 2
+        o.side2 <- 1
+        o.xlab <- "Elevation (m)"
+        # coordinate system selection
+        if ( coorsys == 84 ) {
+            elev <- cbind(elevprof84[, 2], elevprof84[, 1])
+            o.xlim <- c(0,4200)
+            o.ylim <- c(21.5, 25.7)
+            o.ylab <- "Latitude"
+            latdeg <- parse(text=paste(22:25,"*degree~", "N", sep=""))
+            gridv <- 22:25
+            gridv.sub <- 215:257/10 
+            datun.y <- 25.2
+            datun.x <- 1200
+            hsueh.y <- 24.35
+            hsueh.x <- 4000
+            yushan.y <- 23.45
+            yushan.x <- 4000
+            beidawu.y <- 22.6
+            beidawu.x <- 3200
+        } else if ( coorsys == 97 ) {
+            elev <- cbind(elevprof[, 2], elevprof[, 1])
+            o.xlim <- c(0,4200)
+            o.ylim <- c(2380000,2850000)
+            o.ylab <- "TWD97 TM2 Y (meter)"
+            latdeg <- c(24:28*100000)
+            gridv <- 24:28*100000
+            gridv.sub <- 235:287*10000
+            datun.y <- 2790000
+            datun.x <- 1200
+            hsueh.y <- 2700000
+            hsueh.x <- 4000
+            yushan.y <- 2600000
+            yushan.x <- 4000
+            beidawu.y <- 2500000 
+            beidawu.x <- 3200
+        } else print("Unsupported coordinate system!")
+
+    quartz(width=5,height=12)
+    plot(elev, pch=".", ylim=o.ylim, xlim=o.xlim, axes=F,
+                        ylab=o.ylab, xlab=o.xlab)
+    lines(elev)
+    abline(v=c(0:4)*(1000), lwd=1.2, col="grey")
+    abline(v=c(0:42)*(100), lwd=0.5, col="grey")
+    abline(h=gridv,b=0,lwd=1.2,col="grey")
+    abline(h=gridv.sub,b=0,lwd=0.5,col="grey")
+    # draw axis
+    axis(side=o.side1, gridv, latdeg, las=3)
+    axis(side=o.side2, 1000*(0:4), cbind("0","1000","2000","3000","4000"))
+    # draw frame
+    box()
+    if ( mountain == T ) {
+        text(datun.x,datun.y, labels='Datun Shan', srt=90)
+        text(hsueh.x,hsueh.y, labels='Hsueh Shan', srt=90)
+        text(yushan.x,yushan.y, labels='Yu Shan', srt=90)
+        text(beidawu.x,beidawu.y, labels='BeiDawu Shan', srt=90)
+    }
+
+    } else print("orientation=1(Landscape) or 2(Portrait)")
+}
