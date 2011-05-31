6257efd479599002f2bc341256bbb4adafa6b965
Lin, Cheng-Tao <mutolisp@gmail.com>
2011-05-31 14:58:46 +0800

    Add different modes (1: landscape, 2: portrait left, 3: portrait right)  in draw.vertmap,

--- R/twmap.r
+++ R/twmap.r
@@ -276,6 +276,65 @@ draw.vertmap <- function(coorsys, orient=1, mountain=T) {
         text(yushan.x,yushan.y, labels='Yu Shan', srt=90)
         text(beidawu.x,beidawu.y, labels='BeiDawu Shan', srt=90)
     }
+    } else if ( orient == 3 ){
+        o.side1 <- 4 
+        o.side2 <- 1
+        o.xlab <- "Elevation (m)"
+        # coordinate system selection
+        if ( coorsys == 84 ) {
+            elev <- cbind(elevprof84[, 2], elevprof84[, 1])
+            o.xlim <- c(0,4200)
+            o.ylim <- c(21.5, 25.7)
+            o.ylab <- "Latitude"
+            latdeg <- parse(text=paste(22:25,"*degree~", "N", sep=""))
+            gridv <- 22:25
+            gridv.sub <- 215:257/10 
+            datun.y <- 25.2
+            datun.x <- 1200
+            hsueh.y <- 24.35
+            hsueh.x <- 4000
+            yushan.y <- 23.45
+            yushan.x <- 4000
+            beidawu.y <- 22.6
+            beidawu.x <- 3200
+        } else if ( coorsys == 97 ) {
+            elev <- cbind(elevprof[, 2], elevprof[, 1])
+            o.xlim <- c(0,4200)
+            o.ylim <- c(2380000,2850000)
+            o.ylab <- "TWD97 TM2 Y (meter)"
+            latdeg <- c(24:28*100000)
+            gridv <- 24:28*100000
+            gridv.sub <- 235:287*10000
+            datun.y <- 2790000
+            datun.x <- 1200
+            hsueh.y <- 2700000
+            hsueh.x <- 4000
+            yushan.y <- 2600000
+            yushan.x <- 4000
+            beidawu.y <- 2500000 
+            beidawu.x <- 3200
+        } else print("Unsupported coordinate system!")
 
-    } else print("orientation=1(Landscape) or 2(Portrait)")
+    par(oma=c(0,0,0,4))
+    plot(elev, pch=".", ylim=o.ylim, xlim=o.xlim, axes=F,
+                        ylab="", xlab=o.xlab)
+    mtext(o.ylab, side=4, las=3, outer=T)
+    lines(elev)
+    abline(v=c(0:4)*(1000), lwd=1.2, col="grey")
+    abline(v=c(0:42)*(100), lwd=0.5, col="grey")
+    abline(h=gridv,b=0,lwd=1.2,col="grey")
+    abline(h=gridv.sub,b=0,lwd=0.5,col="grey")
+    # draw axis
+    axis(side=o.side1, gridv, latdeg, las=3)
+    axis(side=o.side2, 1000*(0:4), cbind("0","1000","2000","3000","4000"))
+    # draw frame
+    box()
+    if ( mountain == T ) {
+        text(datun.x,datun.y, labels='Datun Shan', srt=90)
+        text(hsueh.x,hsueh.y, labels='Hsueh Shan', srt=90)
+        text(yushan.x,yushan.y, labels='Yu Shan', srt=90)
+        text(beidawu.x,beidawu.y, labels='BeiDawu Shan', srt=90)
+    }
+
+    } else print("orientation=1(Landscape)/2(Portrait left)/3(Portrait right)")
 }
